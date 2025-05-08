345b7f9731b1750b45e8fd7a9f177aa626216504
sheetal sonsale <152849597+sheetalsonsale@users.noreply.github.com>
2025-05-08 18:31:15 +0000

    create get quize api

--- quizze/home/urls.py
+++ quizze/home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home)
+    path('',views.home),
+    path('api/get_quizze',views.get_quiz,name='get_quizze')
 ]
--- quizze/home/views.py
+++ quizze/home/views.py
@@ -1,6 +1,36 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
+from .models import *
+import random
 
 # Create your views here.
 def home(request):
     return HttpResponse('hello from me ')
+
+# {
+#     'status':True
+#     'data':[{},]
+
+# }
+
+def get_quiz(request):
+    try:
+        question_objs = list(Question.objects.all())
+       
+        data = []
+        random.shuffle((question_objs))
+        for question_obj in question_objs:
+            data.append({
+                'category':question_obj.category.category_name,
+                'question':question_obj.question,
+                'marks':question_obj.marks
+            })
+
+        payload = {'status':True,'data':data}
+
+        return JsonResponse(payload)
+        
+
+    except Exception as e:
+        print(e)
+    return HttpResponse('something went Wong')
